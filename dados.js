f195a257d9b5da4698adf5e0446dc1f6ef4df260
danubiocabral <106486212+danubiocabral@users.noreply.github.com>
2024-09-05 15:09:02 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,18 @@
+let dados = /*"Dados do Ensino Técnico no Piauí"*/[
+    {
+        titulo: "SENAC-PI",
+        descricao: "O SENAC oferece uma ampla variedade de cursos técnicos nas áreas de comércio, serviços e gestão, com foco no desenvolvimento de habilidades para o mercado de trabalho. Alguns exemplos de cursos oferecidos pelo SENAC são: Administração, Marketing, Recursos Humanos, Gastronomia e Hotelaria.",
+        link: "https://www.pi.senac.br/"
+    },
+    {
+        titulo: "SENAI-PI",
+        descricao: "O SENAI oferece cursos técnicos nas áreas de indústria, preparando profissionais para o mercado de trabalho industrial. Alguns exemplos de cursos oferecidos pelo SENAI são: Mecânica, Eletrotécnica, Automação Industrial, Programação e Desenvolvimento de Sistemas.",
+        link: "https://fiepi.com.br/"
+    },
+    {
+        titulo: "SEDUC-PI",
+        descricao: "A SEDUC-PI oferece cursos técnicos nas escolas estaduais, abrangendo diversas áreas do conhecimento. Alguns exemplos de cursos oferecidos pela SEDUC são: Agropecuária, Informática, Enfermagem e Administração.",
+        link: "https://www.seduc.pi.gov.br/"
+    },
+];
+
